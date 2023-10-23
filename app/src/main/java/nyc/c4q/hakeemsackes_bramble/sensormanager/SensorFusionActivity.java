b8403dc7f074108ef6fb83bba858991ab881d3af
hakeemSackesbramble <hakksaxx@outlook.com>
2023-10-22 21:00:45 -0400

    build updates

--- app/src/main/java/nyc/c4q/hakeemsackes_bramble/sensormanager/SensorFusionActivity.java
+++ app/src/main/java/nyc/c4q/hakeemsackes_bramble/sensormanager/SensorFusionActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -116,5 +117,19 @@ public class SensorFusionActivity extends AppCompatActivity implements SensorEve
         }
         pageRefresher();
         return true;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
